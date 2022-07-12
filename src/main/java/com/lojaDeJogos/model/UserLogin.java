e29409077b3e19f2b2e06208a2dea2f3853aa4cc
nathaliacrz <nathalias161@gmail.com>
2022-07-12 10:42:23 -0300

    adicionando security e service

---
+++ src/main/java/com/lojaDeJogos/model/UserLogin.java
@@ -0,0 +1,64 @@
+package com.lojaDeJogos.model;
+
+public class UserLogin {
+	
+	private Long id;
+	
+	private String nome;
+
+	private String usuario;
+
+	private String foto;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	private String senha;
+
+	private String token;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
---
+++ src/main/java/com/lojaDeJogos/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.lojaDeJogos.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lojaDeJogos.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+}
